dece37ff6cb9798fb2190402c01c187bb0af3dee
Shahidul Islam <shawon_math07@yahoo.com>
2016-10-21 23:51:24 +0600

    variable name clash fixed

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php 
 
 		# code...
-		$from=$_POST['fromdestination'];
-		$to=$_POST['todestination'];
+		$fromdestination=$_POST['fromdestination'];
+		$todestination=$_POST['todestination'];
 		$fname=$_POST['fname'];
 		$lname=$_POST['lname'];
 		$email=$_POST['email'];
@@ -10,11 +10,11 @@
 		$passportno=$_POST['passportno'];
 		$address=$_POST['address'];
 
-		echo $from,$to,$fname,$lname,$email,$contactno,$passportno,$address;
+		echo $fromdestination,$todestination,$fname,$lname,$email,$contactno,$passportno,$address;
 
 	$to="shawonis08@gmail.com";
 	$subject="clickforFly";
-	$body="$from,$to,$fname,$lname,$email,$contactno,$passportno,$address;";
+	$body="From :$fromdestination,\n To :$todestination,$fname,$lname,$email,$contactno,$passportno,$address;";
 
 	mail($to, $subject, $body);
 	echo "message sent"; 
